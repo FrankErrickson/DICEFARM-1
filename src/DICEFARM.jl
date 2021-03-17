bf5d67cacbe132e9ba05040561047e28666548b0
Frank C. Errickson <fce@princeton.edu>
2021-03-17 07:50:16 -0400

    More clean up for DICEFARM.jl

--- src/DICEFARM.jl
+++ src/DICEFARM.jl
@@ -16,34 +16,47 @@ include(joinpath("components", "DICE", "welfare_component.jl"))
 include(joinpath("components", "farm_component.jl"))
 
 
-# p = dice and farm parameters
-# start_year = first year to run FAIR (1765)
-# end year = last year to run FAIR coupled to DICE+FARM (2500)
-# start_dice_year = first year to switch dice on and fully couple to fair.
+
+#######################################################################################################################
+# Initialize the DICE-FARM Model.
+#######################################################################################################################
+# Description: This function creates the necessary data scenarios and then couples together componets from FAIR,
+#              DICE, and the FARM emissions component. This is just a first-step initialization. In the second step
+#              (below), DICE-FARM harmonizes the endogenous agriculture emissions with the exogenous scenarios to
+#              avoid any double-counting.
+#
+# Function Arguments:
+#
+#       p               = Parameters for the DICE and FARM models.
+#       start_year      = First year to run the model (corresponding to when FAIR starts in 1765).
+#       end_year        = The last year to run FAIR coupled to DICE+FARM (2500).
+#       start_dice_year = The first year to switch DICE on and fully couple it to FAIR (2015).
+#       TCR             = Transient climate response.
+#       ECS             = Equilibrium climtae sensitivity.
+#----------------------------------------------------------------------------------------------------------------------
 
 function initialize_dice_farm(p, start_year, end_year, start_dice_year, TCR, ECS)
 
     #--------------------------------------------------------------------------------------------------------------------
-    # Carry out some data prep.
+    # Carry out data preparation step.
     #--------------------------------------------------------------------------------------------------------------------
 
-    # Get an instance of DICE2016 to access some data.
+    # Get an instance of DICE2016 to access emissions data.
     dice2016 = MimiDICE2016.get_model()
     run(dice2016)
 
-    # Get DICE2016 CO₂ emissions in 2015 and convert units to GtC.
+    # Get DICE2016 total and land-use CO₂ emissions in 2015 and convert units to GtC.
     dice_landuseco2_2015 = dice2016[:emissions, :ETREE][1] * 12.01/44.01
     dice_totalco2_2015   = dice2016[:emissions, :E][1] * 12.01/44.01
 
-    # Create annual MIU, savings rate, TFP values, and land use emissions (cropped from 2015:2500 to match RCP scenario end year, otherwise DICE runs from 2015-2510).
-    # First calculate index for DICE end year that matches with FAIR (in this case, 2500).
+    # Create annual MIU, savings rate, TFP values, and land use emissions (cropped from 2015:2500 to match RCP scenario end year).
     dice_2500_index = findfirst(x -> x == 2500, collect(2015:2510))
     annual_savings  = dice_interpolate(p[:S], 5)[1:dice_2500_index]
     annual_MIU      = dice_interpolate(p[:MIU], 5)[1:dice_2500_index]
     annual_TFP      = dice_interpolate(p[:tfp], 5)[1:dice_2500_index]
     annual_ETREE    = dice_interpolate(p[:etree], 5)[1:dice_2500_index]
 
-    # Pad annualized paramters so the have the time length of the full DICE-FARM model, not juts DICE.
+    # Pad annualized paramters so they have the time length of the full DICE-FARM model, not just DICE.
     annual_savings = pad_parameter(annual_savings, end_year - start_dice_year + 1, start_dice_year - start_year, 0)
     annual_MIU     = pad_parameter(annual_MIU, end_year - start_dice_year + 1, start_dice_year - start_year, 0)
     annual_TFP     = pad_parameter(annual_TFP, end_year - start_dice_year + 1, start_dice_year - start_year, 0)
@@ -57,26 +70,26 @@ function initialize_dice_farm(p, start_year, end_year, start_dice_year, TCR, ECS
     run(m)
 
     # Extract some emissions values from FAIR.
-    rcp_landuse_co2 = m[:landuse_rf, :landuse_emiss]   # Landuse only used for albedo change forcing (GtC).
-    rcp_total_co2   = m[:co2_cycle, :E]                # Total FAIR CO₂ emissions = fossil + landuse (GtC).
-    rcp_fossil_ch4  = m[:ch4_cycle, :fossil_emiss_CH₄] # Fossil sources of methane (Mt CH₄/yr).
-    rcp_fossil_n2o  = m[:n2o_cycle, :fossil_emiss_N₂O] # Fossil sources of nitrous oxide (Mt N/yr).
+    rcp_landuse_co2 = m[:landuse_rf, :landuse_emiss]
+    rcp_total_co2   = m[:co2_cycle, :E]
+    rcp_fossil_ch4  = m[:ch4_cycle, :fossil_emiss_CH₄]
+    rcp_fossil_n2o  = m[:n2o_cycle, :fossil_emiss_N₂O]
 
-    # Calculate RCP index to begin interpolation so there's a smooth transition from historic RCP to present day DICE CO₂ emissions.
+    # Calculate RCP index to begin interpolation so there's a smooth transition from historic RCP to present day DICE CO₂ emissions (2010-2015).
     rcp_years      = collect(1765:2500)
     rcp_2010_index = findfirst(x-> x == 2010, rcp_years)
     rcp_2015_index = findfirst(x-> x == 2015, rcp_years)
 
-    # Calculate the interpolation piece for landuse and fossil CO₂ emissions (from 2010 to 2015).
+    # Calculate the interpolation piece for landuse and fossil CO₂ emissions.
     landuse_interp = dice_interpolate([rcp_landuse_co2[rcp_2010_index], dice_landuseco2_2015], 5)
     total_interp   = dice_interpolate([rcp_total_co2[rcp_2010_index], dice_totalco2_2015], 5)
 
-    # Set up backup emissions scenarios (Mimi requires a backup value for when two compoennts do not overlap).
-    # Will use: RCP emissions (1765-2009), interpolation values (2010-2014), then -9999.99 so an error occurs if model coupling is incorrect (2015 onward is endogenous DICEFARM emissions).
+    # Set up backup emissions scenarios (Mimi requires backup values for when two model compoennts do not overlap).
+    # This uses: RCP emissions (1765-2009), interpolation values (2010-2014), then -9999.99 so an error occurs if model coupling is incorrect (2015 onward is endogenous DICEFARM emissions).
     backup_landuse_RCPco2 = vcat(rcp_landuse_co2[1:(rcp_2010_index-1)], landuse_interp[1:5], ones(length(2015:2500)).*-9999.99)
     backup_total_RCPco2   = vcat(rcp_total_co2[1:(rcp_2010_index-1)], total_interp[1:5], ones(length(2015:2500)).*-9999.99)
 
-    # Set up "backup" CH4 and N2O emissions. No need to interpolate since once DICE FARM kicks in, CH4 and N2O emissions will be endogenous farm emissions + remaining RCP emissions (so they sum to total RCP emissions).
+    # Set up "backup" CH₄ and N₂O emissions. No need to interpolate since once DICE-FARM kicks in, CH₄ and N₂O emissions will be endogenous farm emissions + remaining RCP emissions (so they sum to total RCP emissions).
     backup_fossil_RCPn2o = vcat(rcp_fossil_n2o[1:(rcp_2015_index-1)], ones(length(2015:2500)).*-9999.99)
     backup_fossil_RCPch4 = vcat(rcp_fossil_ch4[1:(rcp_2015_index-1)], ones(length(2015:2500)).*-9999.99)
 
@@ -191,11 +204,27 @@ function initialize_dice_farm(p, start_year, end_year, start_dice_year, TCR, ECS
 
     connect_param!(m, :welfare, :CPC, :neteconomy, :CPC)
 
-    # Return initialized version of model.
+    # Return initialized version of DICE-FARM.
     return m
 end
 
 
+
+#######################################################################################################################
+# Create final version of the DICE-FARM Model.
+#######################################################################################################################
+# Description: This function sets up a final version of DICE+FARM with user-specified parameters and then harmonizes the
+#              endogenous agriculture and exogenous emissions to prevent any double-counting.
+#
+# Function Arguments:
+#
+#       start_year      = First year to run the model (corresponding to when FAIR starts in 1765).
+#       end_year        = The last year to run FAIR coupled to DICE+FARM (2500).
+#       start_dice_year = The first year to switch DICE on and fully couple it to FAIR (2015).
+#       TCR             = Transient climate response.
+#       ECS             = Equilibrium climtae sensitivity.
+#       datafile        = Path to DICE and FARM parameter file.
+#----------------------------------------------------------------------------------------------------------------------
 
 function create_dice_farm(;start_year=1765, end_year=2500, start_dice_year=2015, TCR::Float64=1.69, ECS::Float64=3.1, datafile=joinpath(dirname(@__FILE__), "..", "data", "DICE2016_Excel.xlsm"))
 
@@ -209,17 +238,17 @@ function create_dice_farm(;start_year=1765, end_year=2500, start_dice_year=2015,
     # Need to subtract endogenous FARM CO₂ emissions from exogenous DICE land-use emissions to avoid double-counting.
     new_etree = dice_farm[:emissions, :ETREE] .- dice_farm[:emissions, :Co2EFarm]
 
-    # Subtract agriculture CH4 emissions from exogenous RCP values to avoid double-counting (need to convert FARM emissions from from kg to Mt)
+    # Subtract agriculture CH₄ emissions from exogenous RCP values to avoid double-counting (need to convert FARM emissions from from kg to Mt)
     new_rcp_CH₄ = dice_farm[:emissions, :MethERCP] .- (dice_farm[:emissions, :MethEFarm] / 1e9)
 
-    # Subtract agriculture N2O emissions from exogenous RCP values to avoid double-counting (need to convert FARM emissions from from kg to Mt and from N2O -> N)
+    # Subtract agriculture N₂O emissions from exogenous RCP values to avoid double-counting (need to convert FARM emissions from from kg to Mt and from N₂O -> N)
     new_rcp_N₂O = dice_farm[:emissions, :N2oERCP] .- (dice_farm[:emissions, :N2oEFarm] / 1e9 * (28.01/44.01))
 
     # Update exogenous (non-FARM agriculture) emission sources for land use CO₂, CH₄, and N₂O.
-    update_param!(dice_farm, :ETREE, new_etree)
-    update_param!(dice_farm, :N2oERCP, new_rcp_N₂O)
+    update_param!(dice_farm, :ETREE,    new_etree)
+    update_param!(dice_farm, :N2oERCP,  new_rcp_N₂O)
     update_param!(dice_farm, :MethERCP, new_rcp_CH₄)
 
-    # Return model with updated emission scenarios that fully endogenize the FARM emissions.
+    # Return model with updated emission scenarios that fully endogenize the FARM component emissions.
     return dice_farm
 end
